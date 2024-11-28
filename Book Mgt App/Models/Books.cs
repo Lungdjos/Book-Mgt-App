3f5a60b73c4950bb4f53d625a39302097a4a3b9a
Lungu David <lungudjos@gmail.com>
2024-11-28 15:43:31 +0200

    adding the Books model file

--- Book Mgt App/Models/Books.cs
+++ Book Mgt App/Models/Books.cs
@@ -1,9 +1,13 @@
-﻿namespace Book_Mgt_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Book_Mgt_App.Models
 {
     public class Books
     {
+        [Key]
         public int Id { get; set; }
         public required string Title { get; set; }
+        public required string Description { get; set; }
         public required string Author { get; set; }
         public required string ISBN { get; set; }
     }
