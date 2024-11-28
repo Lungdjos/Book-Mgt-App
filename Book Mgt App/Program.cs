b3671494eb55b96ae8e43702108f298765b64f37
Lungu David <lungudjos@gmail.com>
2024-11-28 15:42:45 +0200

    adding the db link up

--- Book Mgt App/Program.cs
+++ Book Mgt App/Program.cs
@@ -1,7 +1,12 @@
+using Book_Mgt_App.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
