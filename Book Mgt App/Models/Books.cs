6e33ec1fefe27fde950b406a913caaf821b1ceff
Lungu David <lungudjos@gmail.com>
2024-11-28 15:23:45 +0200

    Add project files.

---
+++ Book Mgt App/Models/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace Book_Mgt_App.Models
+{
+    public class Books
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Author { get; set; }
+        public required string ISBN { get; set; }
+    }
+}
