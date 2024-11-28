4e2ff11b2b51ae0794a2aed54e1216d832e22bb6
Lungu David <lungudjos@gmail.com>
2024-11-28 15:43:57 +0200

    Added and updated files for Book Management App

---
+++ Book Mgt App/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Book_Mgt_App.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Book_Mgt_App.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Books> Books { get; set; }
+    }
+}
